fe8932eaec232141b620e22f74c297ca2ee4be94
Hao Yuan <haomichaelyuan@gmail.com>
2022-12-16 13:10:41 +1100

    commit tree 8bb33ba0abffedd0ce3a095b771655021daf1aef

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 // Your code goes here
+// document.addEventListener('DOMContentLoaded',()=>{
+//     const p=document.querySelector('p')
+//     p.textContent='This is really cool!'
+// })
+
 document.addEventListener('DOMContentLoaded',()=>{
-    const p=document.querySelector('p')
-    p.textContent='This is really cool!'
-})
+    const text=document.getElementById('text')
+    text.textContent='This is really cool!'
+}
+    
+   
+)
+console.log(
+    'This console.log()fires when index.js loads - before DOMContentLoaded is triggered'
+)
